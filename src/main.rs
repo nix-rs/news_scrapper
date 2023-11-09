9d0b40aabf7628207425fe3b6f086ac99a8fba62
aniket <aniket2contact@gmail.com>
2023-11-09 23:45:17 +0530

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use select::document::Document;
+use select::predicate::Name;
+use reqwest;
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://edition.cnn.com/";
+
+    let mut resp = reqwest::get(url).unwrap();
+    assert!(resp.status().is_success());
+
+    Document::from_read(resp)
+        .unwrap()
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
 }
